964431fe5cc7cb5c9aec6da38bd7ac1c1b519ac5
Aikodesu11 <61793996+PinPuPii11@users.noreply.github.com>
2022-04-16 16:36:16 +0000

    Course Details

--- logout.js
+++ logout.js
@@ -1,38 +1,82 @@
 Name = localStorage.getItem("myName");
-var resetValue = 0;
 document.getElementById("name").innerHTML = Name;
 
-const subject = {
-  "TU100": {name: "Civic Engagement", code: "TU100", thDes:"ปลูกฝังจิตสํานึก บทบาท และหน้าที่ความรับผิดชอบของการเป็นสมาชิกที่ดีของสังคมในฐานะพลเมืองโลกผ่านกระบวนการหลากหลายวิธี เช่น การบรรยาย การอภิปรายกรณีศึกษาต่างๆ ดูงานเป็นต้น โดยนักศึกษาจะต้องจัดทําโครงการรณรงค์ เพื่อให้เกิดการรับรู้ หรือเกิดการเปลี่ยนแปลง ในประเด็นที่สนใจ", enDes: "Instillation of social conscience and awareness of one’s role and duties as a good global citizen. This is done through a variety of methods such as lectures, discussion of various case studies and field study outings. Students are required to organise a campaign to raise awareness or bring about change in an area of their interest.", section: ["760001","760002","760003"]},
-  "TU102": {name: "Social Life Skills", code: "TU102", thDes: "การดุแลสุขภาพตนเองแบบองค์รวม ทั้งทางด้านร่างกาย อารมณ์ สังคม และจิตวิญญาณ ซึ่งเป็นทักษะสำคัญที่จะช่วยให้ประสบความสำเร็จและใช้ชีวิตในสังคมอย่างเป็นสุข ด้วยการพัฒนาความสามารถในการดูแลสุขภาพทางกายการจัดการความเครียด การสร้างความมั่นคงทางอารมณ์ การเข้าใจตนเองและการปรับตัวเมื่อเผชิญกับปัญหาทางด้านจิตใจ อารมณ์ และสังคม การเข้าใจความหมายของสุนทรียศาสตร์ การได้รับประสบการณ์และความซาบซึ้งในความสัมพันธ์ระหว่างศิลปะกับมนุษย์ ในแขนงต่างๆ ทั้งทัศนศิลป์ ดนตรี ศิลปะการแสดง และสถาปัตยกรรม", enDes: "Holistic health care, addressing the physical ,emotional ,social ,and spiritual needs, which is considered. Important skills for success in leading a happy life in society. Students learn to develop their ability in physical health care to manage stress, build emotional security, understand themselves and adapt to psychological, emotional and social problems. Students also learn to understand the meaning of aesthetics, experiencing and appreciating the relationship between art and humanity in different fields, namely visual arts, music, performing arts and architecture.", section: ["760001","760002","760003"]},
-  "TU109": {name: "Innovation and Entrepreneurial Mindset", code: "TU109", thDes: "การประเมินความเสี่ยงและการสร้างโอกาสใหม่ การคิดและการวางแผนแบบผู้ประกอบการ การตัดสินใจและการพัฒนาธุรกิจ การสื่อสารเชิงธุรกิจและการสร้างแรงจูงใจอย่างมีประสิทธิภาพ การสร้างคุณค่าร่วมเพื่อสังคม", enDes: "Risk assessment and creating new opportunities. Thinking and planning as an entrepreneur. Decision making and entrepreneurial venture development. Business communication for delivering concept or initiative in an efficient, effective and compelling manner. Social shared value creation.", section: ["760001","760002","760003"]},
-  "TU107": {name: "Digital Skill and Problem Solving", code: "TU107", thDes: "ทักษะการคิดเชิงค านวณเพื่อการแก้ปัญหาและการพัฒนาโอกาสใหม่ด้านสังคมและเศรษฐกิจ ความสามารถในค้นหาและการเข้าถึงสารสนเทศได้อย่างมีประสิทธิภาพ การประเมินความน่าเชื่อถือของสารสนเทศ การกลั่นกรองและจัดการสารสนเทศอย่างเป็นระบบ การใช้และจรรยาบรรณด้านดิจิทัล การสื่อสารออนไลน์อย่างมือาชีพ", enDes: "Basic computational thinking skill for solving problems and developing new social and economic opportunities. Efficient access and search for information. Information reliability evaluation. Filtering and managing information systematically. Ethical digital usage and professional online communication.", section: ["760001","760002","760003"]},
-  "SF210": {name: "Programming Skill Development 1", code: "SF210", thDes:"การเขียนโปรแกรมและการแก้ปัญหาด้วยภาษาไพธอน เน้นหลักการของการพัฒนาซอฟแวร์รูปแบบการเขียนโค้ด และการทดสอบ หัวข้อประกอบด้วย กระบวนค าสั่งและฟังก์ชั่น การวนซ้ำ การเรียกซ้ำ อาร์เรย์และเวกเตอร์ สตริง รูปแบบการดำเนินงานของการเรียกใช้กระบวนคำสั่งและฟังก์ชั่น ขั้นตอนวิธี ความผิดปกติ", enDes: "Programming and problem solving using Python. Emphasizes principles of software development, style, and testing. Topics include procedures and functions, iteration, recursion, arrays and vectors, strings, an operational model of procedure and function calls, algorithms, exceptions.", section: ["760001","760002","760003"]}};
 
-function Logout(){
-  localStorage.setItem("myName", resetValue);
+var logout = document.getElementById("logout");
+logout.onclick = function(){
+  localStorage.removeItem("myName");
+  localStorage.removeItem("Subject");
+  localStorage.removeItem("role");
   window.location = "index.html";
+}
+
+const subject = [{quota:50,thName: "มธ.100 พลเมืองกับการลงมือแก้ปัญหา",enName: "TU100 Civic Engagement", code: "TU100", thDes:"ปลูกฝังจิตสํานึก บทบาท และหน้าที่ความรับผิดชอบของการเป็นสมาชิกที่ดีของสังคมในฐานะพลเมืองโลกผ่านกระบวนการหลากหลายวิธี เช่น การบรรยาย การอภิปรายกรณีศึกษาต่างๆ ดูงานเป็นต้น โดยนักศึกษาจะต้องจัดทําโครงการรณรงค์ เพื่อให้เกิดการรับรู้ หรือเกิดการเปลี่ยนแปลง ในประเด็นที่สนใจ", enDes: "Instillation of social conscience and awareness of one’s role and duties as a good global citizen. This is done through a variety of methods such as lectures, discussion of various case studies and field study outings. Students are required to organise a campaign to raise awareness or bring about change in an area of their interest.",section: ["760001","760002","760003"]},
+{quota:50,thName: "มธ.102 ทักษะชีวิตทางสังคม",enName: "TU102 Social Life Skills", code: "TU102", thDes: "การดุแลสุขภาพตนเองแบบองค์รวม ทั้งทางด้านร่างกาย อารมณ์ สังคม และจิตวิญญาณ ซึ่งเป็นทักษะสำคัญที่จะช่วยให้ประสบความสำเร็จและใช้ชีวิตในสังคมอย่างเป็นสุข ด้วยการพัฒนาความสามารถในการดูแลสุขภาพทางกายการจัดการความเครียด การสร้างความมั่นคงทางอารมณ์ การเข้าใจตนเองและการปรับตัวเมื่อเผชิญกับปัญหาทางด้านจิตใจ อารมณ์ และสังคม การเข้าใจความหมายของสุนทรียศาสตร์ การได้รับประสบการณ์และความซาบซึ้งในความสัมพันธ์ระหว่างศิลปะกับมนุษย์ ในแขนงต่างๆ ทั้งทัศนศิลป์ ดนตรี ศิลปะการแสดง และสถาปัตยกรรม", enDes: "Holistic health care, addressing the physical ,emotional ,social ,and spiritual needs, which is considered. Important skills for success in leading a happy life in society. Students learn to develop their ability in physical health care to manage stress, build emotional security, understand themselves and adapt to psychological, emotional and social problems. Students also learn to understand the meaning of aesthetics, experiencing and appreciating the relationship between art and humanity in different fields, namely visual arts, music, performing arts and architecture.", section: ["760001","760002","760003"]},
+{quota:50,thName: "มธ.109 นวัตกรรมกับกระบวนคิดผู้ประกอบการ",enName: "Innovation and Entrepreneurial Mindset", code: "TU109", thDes: "การประเมินความเสี่ยงและการสร้างโอกาสใหม่ การคิดและการวางแผนแบบผู้ประกอบการ การตัดสินใจและการพัฒนาธุรกิจ การสื่อสารเชิงธุรกิจและการสร้างแรงจูงใจอย่างมีประสิทธิภาพ การสร้างคุณค่าร่วมเพื่อสังคม", enDes: "Risk assessment and creating new opportunities. Thinking and planning as an entrepreneur. Decision making and entrepreneurial venture development. Business communication for delivering concept or initiative in an efficient, effective and compelling manner. Social shared value creation.", section: ["760001","760002","760003"]},
+{quota:50,thName: "มธ.107 ทักษะดิจิทัลกับการแก้ปัญหา",enName: "TU107 Digital Skill and Problem Solving", code: "TU107", thDes: "ทักษะการคิดเชิงค านวณเพื่อการแก้ปัญหาและการพัฒนาโอกาสใหม่ด้านสังคมและเศรษฐกิจ ความสามารถในค้นหาและการเข้าถึงสารสนเทศได้อย่างมีประสิทธิภาพ การประเมินความน่าเชื่อถือของสารสนเทศ การกลั่นกรองและจัดการสารสนเทศอย่างเป็นระบบ การใช้และจรรยาบรรณด้านดิจิทัล การสื่อสารออนไลน์อย่างมือาชีพ", enDes: "Basic computational thinking skill for solving problems and developing new social and economic opportunities. Efficient access and search for information. Information reliability evaluation. Filtering and managing information systematically. Ethical digital usage and professional online communication.", section: ["760001","760002","760003"]},
+{quota:50,thName: "วซ.210 การพัฒนาการเขียนโปรแกรมคอมพิวเตอร์ 1",enName: "SF210 Programming Skill Development 1", code: "SF210", thDes: "การเขียนโปรแกรมและการแก้ปัญหาด้วยภาษาไพธอน เน้นหลักการของการพัฒนาซอฟแวร์รูปแบบการเขียนโค้ด และการทดสอบ หัวข้อประกอบด้วย กระบวนค าสั่งและฟังก์ชั่น การวนซ้ำ การเรียกซ้ำ อาร์เรย์และเวกเตอร์ สตริง รูปแบบการดำเนินงานของการเรียกใช้กระบวนคำสั่งและฟังก์ชั่น ขั้นตอนวิธี ความผิดปกติ", enDes: "Programming and problem solving using Python. Emphasizes principles of software development, style, and testing. Topics include procedures and functions, iteration, recursion, arrays and vectors, strings, an operational model of procedure and function calls, algorithms, exceptions.", section: ["760001","760002","760003"]}, 
+{quota:50,thName: "มธ.104 การคิด อ่าน และเขียนอย่างมีวิจารณญาณ", enName: "TU104 Critical Thinking, Reading, and Writing", code: "TU104", thDes: "พัฒนาทักษะการคิดอย่างมีวิจารณญาณผ่านการตั้งค าถาม การวิเคราะห์ การสังเคราะห์ และการประเมินค่า พัฒนาทักษะการอ่านเพื่อจับสาระสำคัญ เข้าใจจุดมุ่งหมาย ทัศนคติ สมมติฐาน หลักฐานสนับสนุนการใช้เหตุผลที่นำไปสู่ข้อสรุปของงานเขียน พัฒนาทักษะการเขียนแสดงความคิดเห็นอย่างมีเหตุผลและการเขียนเชิงวิชาการ รู้จักถ่ายทอดความคิด และเชื่อมโยงข้อมูลเข้ากับมุมมองของตนเอง รวมถึงสามารถอ้างอิงหลักฐานและข้อมูลมาใช้ในการสร้างสรรค์งานเขียนได้อย่างมีประสิทธิภาพ", enDes: "Development of critical thinking through questioning, analytical, synthetic and evaluation skills. Students learn how to read without necessarily accepting all the information presented in the text, but rather consider the content in depth, taking into account the objectives, perspectives, assumptions, bias and supporting evidence, as well as logic or strategies leading to the author’s conclusion. The purpose is to apply these methods to students’ own persuasive writing based on information researched from various sources, using effective presentation techniques.", section: ["760001","760002","760003"]},
+{quota:50,thName: "ค.111 แคลคูลัสพื้นฐาน", enName: "MA111 Fundamentals of Calculus", code: "MA111", thDes:"อุปนัยเชิงคณิตศาสตร์ ระบบจำนวนและฟังก์ชันเบื้องต้น แคลคูลัสอนุพันธ์และปริพันธ์ของฟังก์ชันตัวแปรเดียว ลิมิตความต่อเนื่อง อนุพันธ์ และการประยุกต์อนุพันธ์ ปฏิยานุพันธ์ เทคนิคการหาปริพันธ์และการประยุกต์ปริพันธ์ ปริพันธ์ไม่ตรงแบบ อนุกรม ทฤษฎีของเทย์เลอร์ของฟังก์ชันพื้นฐาน การหาปริพันธ์เชิงตัวเลขเบื้องต้น หมายเหตุ : ไม่นับหน่วยกิตให้ผู้ที่กำลังศึกษาหรือสอบได้ ค.211 หรือ ค.216 หรือ ค.218 หรือ คป.101", enDes: "Mathematical induction, number systems and elementary functions, calculus of one variable functions, limit, continuity, the derivative and its applications, antiderivatives, techniques of integrations and its applications, improper integrals, series, Taylor’s Theorem for basic functions, numerical integration. Note: There is no credit for students who are currently taking or have earned credits of MA211 or MA216 or MA 218 or AM101", section: ["760001","760002","760003"]}];
+
+//Set info to each subject.
+for(i=0;i<subject.length;i++){
+  if(localStorage.Subject == subject[i].code){
+    var coursedetailTH = subject[i].thDes;
+    var coursedetailEN = subject[i].enDes;
+    break;
+  }
+}
+document.getElementById("thDes").innerHTML = coursedetailTH;
+document.getElementById("enDes").innerHTML = coursedetailEN;
+
+
+//Check subject.
+function checksubject(sub){
+  subname = document.querySelector("#detail");
+  localStorage.setItem("Subject", sub);
+  window.location = "registerDetailStudent.html";
   
 }
+//Subject name assign when page has jumped
+subname = localStorage.getItem("Subject");
+document.getElementById("detail").innerHTML = subname;
 
-function gotoSubject1() {
-  localStorage.setItem("Code", "TU100");
-  document.getElementById("detail").innerHTML = subject.TU100.thDes;
-  //window.location = "registerDetailStudent.html";
+//When click at Course Register, reset subject.
+let barCourseRegister = document.getElementById("barCourseRegister");
+barCourseRegister.onclick = function(){
+  localStorage.setItem("Subject",'');
 }
 
-function gotoSubject2() {
-  window.location = "registerDetailStudent.html";
+//Edit quota when click register//
+let registerButton = document.getElementById("registerButton");
+registerButton.onclick = function(){
+  var registering = Name + localStorage.getItem("Subject");
+  localStorage.setItem(registering, "Registered");
 }
 
-function gotoSubject3() {
-  window.location = "registerDetailStudent.html";
+//Decrease quota
+function decreasequota(){
+  var key_of_local = localStorage.getItem("Subject") + "quota";
+  localStorage.setItem(key_of_local, (localStorage.getItem(key_of_local)-1));
 }
 
-function gotoSubject4() {
-  window.location = "registerDetailStudent.html";
+// Assign Subject Info to localStorage **This is a part of Admin//
+var assign = document.getElementById("Assign");
+
+assign.onclick = function(){
+  for(i = 0; i<subject.length; i++){
+    var localname = subject[i].code + "quota";
+    localStorage.setItem(localname, subject[i].quota);
+  }
 }
 
+//Variable for quota
 
-if('TU100' == localStorage.Code){
-  document.getElementById("detail").innerHTML = subject.TU100.thDes;
+//Check quota
+// function checkquota(){
+//   for(i = 0;i<localStorage.length;i++){
+//     if(localStorage.key(i).includes(Subject)){
+      
+//     }
+//   }
+// }
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const keyBoard = document.getElementById("username")
 const keyBoard2 = document.getElementById("form-input form_input-error")
-//var username =
 
 keyBoard.addEventListener('keydown', e =>{
   if(e.keyCode === 13){
@@ -17,12 +16,13 @@ keyBoard2.addEventListener('keydown', e =>{
 
 // ----------------------------------------------------- //
 
-var userInfo = [{username:"001",password:"ajarnjack",role:"Admin",name:"AjarnJack"},
-                {username:"641",password:"001",role:"Student",name:"Nutchapon"},
-                {username:"641",password:"002",role:"Student",name:"Karnpitcha"},
-                {username:"641",password:"003",role:"Student",name:"Punyanuch"},
-                {username:"641",password:"004",role:"Student",name:"Peerapat"},
-                {username:"641",password:"005",role:"Student",name:"Mallika"}];
+// Login system //
+var userInfo = [{username:"001",password:"ajarnjack",role:"Admin",name:"AjarnJack",lastname:"How to understand"},
+                {username:"641",password:"001",role:"Student",name:"Nutchapon",lastname:"Kittingam"},
+                {username:"641",password:"002",role:"Student",name:"Karnpitcha",lastname:"Hongegg"},
+                {username:"641",password:"003",role:"Student",name:"Punyanuch",lastname:"Patcharapongpan"},
+                {username:"641",password:"004",role:"Student",name:"Peerapat",lastname:"Suttiprasit"},
+                {username:"641",password:"005",role:"Student",name:"Mallika",lastname:"Thammajit"}];
 
 var Name = '';
 var state = 'Undefine';
@@ -59,9 +59,11 @@ function checkInfo(){
 function checkRole(){
   var role = userInfo[count].role;
   if(role=="Admin"){
+    localStorage.setItem("role","Admin");
     window.location = "manageCourseAdmin.html";
   }
   else{
+    localStorage.setItem("role","Student");
     window.location = "myCourseStudent.html";
   }
 }
